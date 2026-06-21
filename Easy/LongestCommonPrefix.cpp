56c0ae123ddc705a9214f1ebb58140b80b5dfc02
DEEMU HARISH KUMAR <harishdeemu@gmail.com>
2026-06-21 21:26:10 +0530

    Add LongestCommonPrefix class implementation
    Implement a solution to find the longest common prefix among an array of strings.

---
+++ Easy/LongestCommonPrefix.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        sort(strs.begin(), strs.end());
+
+        string first = strs[0];
+        string last = strs[strs.size() - 1];
+
+        string ans = "";
+
+        for (int i = 0; i < min(first.size(), last.size()); i++) {
+            if (first[i] != last[i]) break;
+            ans += first[i];
+        }
+
+        return ans;
+    }
+};
+// //output for lcp:
+// Input: ["flower", "flow", "flight"]
+// Output: "fl"
