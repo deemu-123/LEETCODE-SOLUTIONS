a7e8406e2bf980895f05300d741021d6d6180a89
DEEMU HARISH KUMAR <harishdeemu@gmail.com>
2026-06-23 20:31:07 +0530

    Create risingtemp.sql

---
+++ Easy/risingtemp.sql
@@ -0,0 +1,24 @@
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+OUTPUT:
+Input
+Weather =
+| id | recordDate | temperature |
+| -- | ---------- | ----------- |
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
+Output
+| id |
+| -- |
+| 2  |
+| 4  |
+Expected
+| Id |
+| -- |
+| 2  |
+| 4  |
